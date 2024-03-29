51c64edad6cad12531e4af39aa7107c08ebcfc71
Subramaniyam2003 <valarshan2000@gmail.com>
2024-03-29 13:05:30 +0530

    second commit

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -13,8 +13,8 @@ export default function Footer() {
       </ul>
       <h1 className="font-semibold text-3xl m-4 text-black">Get in Touch With</h1>
       <ul  className="flex justify-center items-center  flex-wrap">
-        <li><a href="https://github.com/subramaniyam2003/"><img className="w-7 h-7 m-2 animate-bounce" src={"./src/assets/githubb_icon.png"}/></a></li>
-        <li><a href="https://www.linkedin.com/in/subramaniyam-s-b29731277/"><img className="w-7 h-7 m-2 animate-bounce" src={"./src/assets/linkedinb_icon.png"}/></a></li>
+        <li><a href="https://github.com/subramaniyam2003/"><img className="w-7 h-7 m-2 animate-bounce" src={"./public/assets/githubb_icon.png"}/></a></li>
+        <li><a href="https://www.linkedin.com/in/subramaniyam-s-b29731277/"><img className="w-7 h-7 m-2 animate-bounce" src={"./public/assets/linkedinb_icon.png"}/></a></li>
     
       </ul>
     </div>
