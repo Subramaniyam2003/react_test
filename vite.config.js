08c1dd7a21fd67025ab6674c6d1c04fe26994251
Subramaniyam2003 <valarshan2000@gmail.com>
2024-03-29 08:13:53 +0530

    svss

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite_react/",
-  plugins: [react()],
+  base:"/react_test/",
+  plugins: [react()]
 })
